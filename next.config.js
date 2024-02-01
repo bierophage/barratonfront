58fced17fb7c3a54654cac2741609ccbe12b37de
bierophage <bierophage@hotmail.com>
2024-02-01 22:40:45 +0100

    paypal

--- next.config.js
+++ next.config.js
@@ -3,14 +3,14 @@ const store = require("./store.config.json")
 const ContentSecurityPolicy = `font-src 'self' js.stripe.com`;
 
 
-const cspHeader = `
-    default-src 'self';
-    script-src 'self' 'unsafe-eval' 'unsafe-inline' 'https://maps.googleapis.com' 'https://*.stripe.com' 'https://connect-js.stripe.com' 'https://checkout.stripe.com' 'https://js.stripe.com' 'https://m.stripe.network';
-    style-src 'self' 'unsafe-inline';
-    img-src 'self' 'https://*.stripe.com';
-    frame-src 'https://checkout.stripe.com' 'https://connect-js.stripe.com' 'https://js.stripe.com' 'https://hooks.stripe.com';
-    connect-src 'https://checkout.stripe.com' 'https://api.stripe.com' 'https://maps.googleapis.com';
-`
+// const cspHeader = `
+//     default-src 'self';
+//     script-src 'self' 'unsafe-eval' 'unsafe-inline' 'https://maps.googleapis.com' 'https://*.stripe.com' 'https://connect-js.stripe.com' 'https://checkout.stripe.com' 'https://js.stripe.com' 'https://m.stripe.network';
+//     style-src 'self' 'unsafe-inline';
+//     img-src 'self' 'https://*.stripe.com';
+//     frame-src 'https://checkout.stripe.com' 'https://connect-js.stripe.com' 'https://js.stripe.com' 'https://hooks.stripe.com';
+//     connect-src 'https://checkout.stripe.com' 'https://api.stripe.com' 'https://maps.googleapis.com';
+// `
 
 /**
  * @type {import('next').NextConfig}
@@ -18,19 +18,19 @@ const cspHeader = `
 const nextConfig = withStoreConfig({
   features: store.features,
   reactStrictMode: true,
-  async headers() {
-    return [
-      {
-        source: '/(.*)',
-        headers: [
-          {
-            key: 'Content-Security-Policy',
-            value: cspHeader.replace(/\n/g, ''),
-          },
-        ],
-      },
-    ]
-  },
+  // async headers() {
+  //   return [
+  //     {
+  //       source: '/(.*)',
+  //       headers: [
+  //         {
+  //           key: 'Content-Security-Policy',
+  //           value: cspHeader.replace(/\n/g, ''),
+  //         },
+  //       ],
+  //     },
+  //   ]
+  // },
   images: {
     remotePatterns: [
       {
